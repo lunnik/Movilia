1493afc85ada58a6094a2a4e1e74e13020f7f883
lunnik <argaed@gmail.com>
2017-02-16 17:09:49 -0600

    se agregaron markers en google maps

--- app/src/main/java/com/lionsquare/movilia/activities/MapsActivity.java
+++ app/src/main/java/com/lionsquare/movilia/activities/MapsActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +18,11 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.lionsquare.movilia.R;
 
+import java.util.HashMap;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap googleMap;
+    private HashMap<Marker, Integer> mHashMap = new HashMap<Marker, Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +41,39 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     Toast.makeText(MapsActivity.this, "Se necesitan permisos", Toast.LENGTH_SHORT).show();
                 }
             }
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
             finish();
         }
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+    }
+
+    void addMaker() {
+        for (int i = 0; i <=1; i++) {
+            Marker marker = googleMap.addMarker(new MarkerOptions().position(new LatLng(19.5066863, -99.2251105)));
+
+            mHashMap.put(marker, i);
+        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap maps) {
         googleMap = maps;
-
+        addMaker();
 
         googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
+                int pos = mHashMap.get(marker);
+
+                Log.i("Position of arraylist", pos + "");
                 return false;
             }
         });
